38c025a0a50702e659aa7e1b658a1d85094dc6c0
p1t1ch <belobeev.kirill@gmail.com>
2021-01-30 12:31:48 +0300

    setup theme

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,2 +1,4 @@
 import 'focus-visible'
+import '@fontsource/inter/latin-400.css'
+import '@fontsource/inter/latin-700.css'
 import '@/styles/global.css'
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,39 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    colors: {
+      primary: {
+        green: '#1EB589',
+        red: '#DC414C',
+      },
+      socials: {
+        facebook: '#178FF5',
+        twitter: '#1DA1F2',
+        'instagram-start': '#DF4896',
+        'instagram-end': '#FDC366',
+        youtube: '#C4032B',
+      },
+      light: {
+        toggle: '#AEB3CB',
+        'blue-pale': '#F7F9FF',
+        'blue-grayish-dark': '#63687D',
+        'blue-grayish-light': '#F1F3FA',
+        'blue-dark': '#1D1F29',
+      },
+      dark: {
+        'toggle-start': '#40DB82',
+        'toggle-end': '#388FE7',
+        'blue-dark': '#20222F',
+        'blue-darkest': '#1D1F29',
+        'blue-desaturated': '#8C98C6',
+        'blue-desaturated-dark': '#252B42',
+      },
+      white: '#FFF',
+    },
+    extend: {
+      fontFamily: {
+        sans: ['Inter', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
 }
